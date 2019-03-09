3f32c6ce2a20f1f6c1c659b54a4804e906282c05
Matt Conway <mattconway727@gmail.com>
2019-03-09 12:05:08 -0600

    added class "change"

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+class Tablink {
+  constructor(link) {
+    this.tabContent = document.querySelector(
+      `.content[data-tab = '${link.dataset.tab}']`
+    );
+    this.tabContent = new Content(this.tabContent);
+    link.addEventListener("click", () => this.linkClick());
+  }
+  linkClick() {
+    this.tabContent.toggleContent();
+  }
+}
+
+class Content {
+  constructor(tabElement) {
+    this.tabElement = tabElement;
+  }
+  toggleContent() {
+    this.tabElement.classList.toggle("change");
+  }
+}
+
+const links = document.querySelectorAll(".link");
+links.forEach(link => new Tablink(link));
